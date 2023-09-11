d2c5ab513875ef255686ec7c77083272bfe5db03
Ali Cagatay <aliccagatay@gmail.com>
2023-09-11 22:18:47 +0100

    Updated `App,js` according to the design images.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
 import "./App.css";
+import QRCode from "./images/image-qr-code.png";
 
-function App() {}
+function App() {
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
+}
+
+function MainComponent() {
+  return (
+    <div className="MainComponent">
+      <ImageComponent />
+      <TextComponent1 />
+      <TextComponent2 />
+    </div>
+  );
+}
+
+function ImageComponent() {
+  return (
+    //Create a new component called ImageComponent which displays an image with the file path src/images/image-qr-code.png
+    <div className="ImageComponent">
+      <img src={QRCode} alt="QR Code" />
+    </div>
+  );
+}
+
+function TextComponent1() {
+  return (
+    <div className="TextComponent1">
+      <p>Improve your front-end skills by building projects</p>
+    </div>
+  );
+}
+function TextComponent2() {
+  return (
+    <div className="TextComponent2">
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </div>
+  );
+}
 
 export default App;
